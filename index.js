abd7224242be9954bc51580008cf909f963d16b9
Sercan Özen <sercangsu@gmail.com>
2023-04-09 16:43:52 +0300

    Update index.js
    added mime types

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const { createFilter } = require("@rollup/pluginutils");
 const {readFileSync} = require("fs");
+import { extname } from 'path';
+
+const mimeTypes = {
+	'.jpg':  'image/jpeg',
+	'.jpeg': 'image/jpeg',
+	'.png':  'image/png',
+	'.gif':  'image/gif'
+	// '.svg':  'image/svg+xml',
+};
+
 
 function base64(opts = {}) {
   if (!opts.include) {
@@ -11,8 +21,9 @@ function base64(opts = {}) {
     name: "base64",
     transform(data, id) {
       if (filter(id)) {
+ 			  const mime = mimeTypes[ extname( id ) ];
         const fileData = readFileSync(id);
-          return  `export default "${fileData.toString('base64')}";`
+          return  `export default "data:${mime};base64,${fileData.toString('base64')}";`
       }
     }
   };
